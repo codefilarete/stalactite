4f66dbc73a6e388b9685b62e2396ca1c34dc76b4
mary <mary@interview-efm.com>
2015-04-02 21:07:22 +0200

    Ajout de test unitaire pour SteppingIterator + doc

--- src/main/java/org/stalactite/lang/collection/SteppingIterator.java
+++ src/main/java/org/stalactite/lang/collection/SteppingIterator.java
@@ -3,6 +3,14 @@ package org.stalactite.lang.collection;
 import java.util.Iterator;
 
 /**
+ * Iterator qui appelle une méthode périodiquement toutes les N itérations.
+ * C'est {@link #hasNext()} qui déclenche l'appel à {@link #onStep()} afin
+ * de correspondre à une boucle while(hasNext) ou foreach. Ce n'est pas {@link #next()}
+ * qui déclenche l'appel car ça semble un peu tard : une incrémentation de trop.
+ * {@link #onStep()} est également appelé quand {@link #hasNext()} renvoie false (sauf
+ * lors du premier appel) afin de traiter le reliquat d'objet depuis le dernier
+ * appel à {@link #onStep()}
+ * 
  * @author Guillaume Mary
  */
 public abstract class SteppingIterator<E> implements Iterator<E> {
---
+++ src/test/java/org/stalactite/lang/collection/SteppingIteratorTest.java
@@ -0,0 +1,100 @@
+package org.stalactite.lang.collection;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Iterator;
+
+import org.junit.Assert;
+import org.mockito.Mockito;
+import org.testng.annotations.Test;
+
+/**
+ * @author Guillaume Mary
+ */
+public class SteppingIteratorTest {
+	
+	@Test
+	public void testHasNext() throws Exception {
+		Iterator<String> iterator = mock(Iterator.class);
+		SteppingIterator testInstance = new SteppingIterator<String>(iterator, 10) {
+			@Override
+			protected void onStep() {
+				
+			}
+		};
+		testInstance.hasNext();
+		verify(iterator).hasNext();
+		testInstance.hasNext();
+		verify(iterator, Mockito.times(2)).hasNext();
+		testInstance.hasNext();
+		verify(iterator, Mockito.times(3)).hasNext();
+	}
+	
+	@Test
+	public void testNext() throws Exception {
+		Iterator<String> iterator = mock(Iterator.class);
+		SteppingIterator testInstance = new SteppingIterator<String>(iterator, 10) {
+			@Override
+			protected void onStep() {
+				
+			}
+		};
+		testInstance.next();
+		verify(iterator).next();
+		testInstance.next();
+		verify(iterator, Mockito.times(2)).next();
+		testInstance.next();
+		verify(iterator, Mockito.times(3)).next();
+	}
+	
+	@Test
+	public void testRemove() throws Exception {
+		Iterator<String> iterator = mock(Iterator.class);
+		SteppingIterator testInstance = new SteppingIterator<String>(iterator, 10) {
+			@Override
+			protected void onStep() {
+				
+			}
+		};
+		testInstance.remove();
+		verify(iterator).remove();
+		testInstance.remove();
+		verify(iterator, Mockito.times(2)).remove();
+		testInstance.remove();
+		verify(iterator, Mockito.times(3)).remove();
+	}
+	
+	@Test
+	public void testOnStep() throws Exception {
+		Iterator<String> iterator = mock(Iterator.class);
+		when(iterator.hasNext()).thenReturn(true);
+		final int[] i= new int[1];
+		SteppingIterator testInstance = new SteppingIterator<String>(iterator, 2) {
+			@Override
+			protected void onStep() {
+				i[0]++;
+			}
+		};
+		testInstance.hasNext();
+		Assert.assertEquals(0, i[0]);
+		testInstance.next();
+		testInstance.hasNext();
+		Assert.assertEquals(0, i[0]);
+		testInstance.next();
+		testInstance.hasNext();
+		Assert.assertEquals(1, i[0]);
+		testInstance.next();
+		testInstance.hasNext();
+		Assert.assertEquals(1, i[0]);
+		testInstance.next();
+		testInstance.hasNext();
+		Assert.assertEquals(2, i[0]);
+		
+		when(iterator.hasNext()).thenReturn(false);
+		testInstance.next();
+		testInstance.hasNext();
+		Assert.assertEquals(3, i[0]);
+	}
+}
