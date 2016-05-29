213c4f6515d59bb5ca70ff3b48f6886fd12a5d82
Guillaume Mary <guiom.mary@gmail.com>
2016-05-29 11:22:36 +0200

    Small FieldIterator enhancement

--- tools/src/main/java/org/gama/lang/Reflections.java
+++ tools/src/main/java/org/gama/lang/Reflections.java
@@ -159,21 +159,30 @@ public final class Reflections {
 		
 		public FieldIterator(ClassIterator classIterator) {
 			this.classIterator = classIterator;
-			this.fieldIterator = new ArrayIterator<>(classIterator.next().getDeclaredFields());
+			this.fieldIterator = new ArrayIterator<>(getFields(classIterator.next()));
+		}
+		
+		/**
+		 * Gives fields of a class. Default is {@link Class#getDeclaredFields()}.
+		 * Can be overriden to filter fields for instance.
+		 * @param clazz the class for which fields must be given
+		 * @return an array of Field, not null
+		 */
+		protected Field[] getFields(Class clazz) {
+			return clazz.getDeclaredFields();
 		}
 		
 		@Override
 		public boolean hasNext() {
 			if (fieldIterator.hasNext()) {
+				// simple case
 				return true;
 			} else {
-				while (!fieldIterator.hasNext()) {
-					if (classIterator.hasNext()) {
-						Field[] declaredFields = classIterator.next().getDeclaredFields();
-						fieldIterator = new ArrayIterator<>(declaredFields);
-					} else {
-						return false;
-					}
+				// no more field for the current iterator => we must scan upper classes if they have fields
+				while (classIterator.hasNext() && !fieldIterator.hasNext()) {
+					// transforming the class fields to an Iterator
+					Field[] declaredFields = getFields(classIterator.next());
+					fieldIterator = new ArrayIterator<>(declaredFields);
 				}
 				return fieldIterator.hasNext();
 			}
--- tools/src/test/java/org/gama/lang/FieldIteratorTest.java
+++ tools/src/test/java/org/gama/lang/FieldIteratorTest.java
@@ -21,30 +21,38 @@ import static org.junit.Assert.assertEquals;
 public class FieldIteratorTest {
 	
 	@DataProvider
-	public static Object[][] testNextMethodsData() {
+	public static Object[][] testNextMethodsData() throws NoSuchFieldException {
+		Field xf1 = X.class.getDeclaredField("f1");
+		Field yf2 = Y.class.getDeclaredField("f2");
+		Field zf2 = Z.class.getDeclaredField("f2");
 		return new Object[][] {
-				{ X.class, Arrays.asList("f1") },
-				{ Y.class, Arrays.asList("f2", "f1") },
-				{ Z.class, Arrays.asList("f2", "f2", "f1") }
+				{ X.class, Arrays.asList(xf1) },
+				{ Y.class, Arrays.asList(yf2, xf1) },
+				{ Z.class, Arrays.asList(zf2, yf2, xf1) },
+				{ NoField.class, Arrays.asList(xf1) }
 		};
 	}
 	
 	@Test
 	@UseDataProvider("testNextMethodsData")
-	public void testNextMethods(Class clazz, List<String> expectedFields) throws Exception {
+	public void testNextMethods(Class clazz, List<Field> expectedFields) throws Exception {
 		FieldIterator testInstance = new FieldIterator(clazz);
-		assertEquals(expectedFields, Iterables.visit(testInstance, new Iterables.ForEach<Field, String>() {
+		assertEquals(expectedFields, Iterables.visit(testInstance, new Iterables.ForEach<Field, Field>() {
 			
 			@Override
-			public String visit(Field field) {
-				return field.getName();
+			public Field visit(Field field) {
+				return field;
 			}
 		}));
 	}
 	
-	static class X { private String f1; }
+	static class X { private Object f1; }
 	
-	static class Y extends X { private String f2; }
+	static class Y extends X { private Object f2; }
 	
-	static class Z extends Y { private String f2; }
+	static class Z extends Y { private Object f2; }
+	
+	static class NoField extends X {
+		
+	}
 }
