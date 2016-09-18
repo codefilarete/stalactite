fb9f5e8faa101f70f347e30624f8a75c8425607b
Guillaume Mary <guiom.mary@gmail.com>
2016-09-18 20:03:23 +0200

    ParameterBinder creation for Identifier centralized since only a small piece of code will change between each

--- orm/src/main/java/org/gama/stalactite/persistence/id/Identifier.java
+++ orm/src/main/java/org/gama/stalactite/persistence/id/Identifier.java
@@ -1,26 +1,43 @@
 package org.gama.stalactite.persistence.id;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.gama.sql.binder.LambdaParameterBinder;
+import org.gama.sql.binder.NullAwareParameterBinder;
+import org.gama.sql.binder.ParameterBinder;
 import org.gama.stalactite.persistence.id.manager.StatefullIdentifier;
-import org.gama.stalactite.persistence.id.provider.IdentifierProvider;
 
 /**
- * A decorator for bean identifier.
- * The surrogate must be persisted, not the instances of this class.
- * 
- * Main purpose is to get ride of attach/detach principle that's in JPA with the aid of {@link IdentifierProvider}
+ * Equivalent of {@link StatefullIdentifier} for a more end-user usage.
+ * Its class hierarchy implements the different cases.
  * 
  * @param <T> the real type of the identifier
  * @author Guillaume Mary
+ * @see PersistableIdentifier
+ * @see PersistedIdentifier
  */
 public interface Identifier<T> extends StatefullIdentifier<T> {
 	
-	T getSurrogate();
-	
 	/**
-	 * Gives the persistence state of this identifier: true means that this identifier is used by a database row and commited in the database.
-	 * False means it's not (which means not used at all or not yet commited).
-	 * 
-	 * @return this implementation always returns true. See {@link PersistableIdentifier} for change made possible.
+	 * Gives a {@link ParameterBinder} for a generic {@link Identifier}
+	 * @param lambdaParameterBinder the surrogate {@link ParameterBinder} (can be for primitive type because null is already handled by this method result)
+	 * @param <I> the type of the surrogate {@link Identifier}
+	 * @return a new {@link ParameterBinder} which will wrap/unwrap the result of lambdaParameterBinder
+	 * @see org.gama.sql.binder.DefaultParameterBinders
 	 */
-	boolean isPersisted();
+	static <I> ParameterBinder<StatefullIdentifier<I>> identifierBinder(LambdaParameterBinder<I> lambdaParameterBinder) {
+		return new NullAwareParameterBinder<>(new ParameterBinder<StatefullIdentifier<I>>() {
+			@Override
+			public StatefullIdentifier<I> get(String columnName, ResultSet resultSet) throws SQLException {
+				return new PersistedIdentifier<>(lambdaParameterBinder.get(columnName, resultSet));
+			}
+			
+			@Override
+			public void set(int valueIndex, StatefullIdentifier<I> value, PreparedStatement statement) throws SQLException {
+				lambdaParameterBinder.set(valueIndex, value.getSurrogate(), statement);
+			}
+		});
+	}
 }
