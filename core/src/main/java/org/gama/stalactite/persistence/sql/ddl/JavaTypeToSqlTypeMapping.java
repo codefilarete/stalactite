5d5a943e21f017ac356ae6c9c16bad1c58efa951
Guillaume Mary <guiom.mary@gmail.com>
2016-12-17 16:51:40 +0100

    Javadoc on JavaTypeToSqlTypeMapping

--- core/src/main/java/org/gama/stalactite/persistence/sql/ddl/JavaTypeToSqlTypeMapping.java
+++ core/src/main/java/org/gama/stalactite/persistence/sql/ddl/JavaTypeToSqlTypeMapping.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
-import org.gama.lang.bean.IFactory;
 import org.gama.lang.collection.Iterables;
 import org.gama.lang.collection.ValueFactoryHashMap;
 import org.gama.stalactite.persistence.structure.Table.Column;
@@ -13,18 +12,22 @@ import org.gama.stalactite.persistence.structure.Table.SizedColumn;
 
 /**
  * Mapping between Java classes and Sql Types.
- * Near Hibernate Dialect::register types principles.
+ * Near Hibernate Dialect::register types principles, so no SQL types are defined here : it's only a storage and finding tool.
  * 
- * @author mary
+ * @see #getTypeName(Class)
+ * @author Guillaume Mary
  */
 public class JavaTypeToSqlTypeMapping {
 	
-	private final Map<Class, SortedMap<Integer, String>> javaTypeToSQLType = new ValueFactoryHashMap<>(new IFactory<Class, SortedMap<Integer, String>>() {
-		@Override
-		public SortedMap<Integer, String> createInstance(Class input) {
-			return new TreeMap<>();
-		}
-	});
+	/**
+	 * SQL types storage per Java type, dedicated to sized-types.
+	 * Values are SortedMaps of size to SQL type. SortedMap are used to ease finding of types per size
+	 */
+	private final Map<Class, SortedMap<Integer, String>> javaTypeToSQLType = new ValueFactoryHashMap<>(input -> new TreeMap<>());
+	
+	/**
+	 * SQL types storage per Java type, usual cases.
+	 */
 	private final Map<Class, String> defaultJavaTypeToSQLType = new HashMap<>();
 	
 	public void put(Class clazz, String sqlType) {
@@ -35,10 +38,16 @@ public class JavaTypeToSqlTypeMapping {
 		javaTypeToSQLType.get(clazz).put(size, sqlType);
 	}
 	
+	/**
+	 * Gives the SQL type name of a column. Main entry point of this class.
+	 *
+	 * @param column a column
+	 * @return the SQL type for the given column
+	 */
 	public String getTypeName(Column column) {
 		Class javaType = column.getJavaType();
 		if (javaType == null) {
-			throw new IllegalArgumentException("Can't give sql type for column "+column.getAbsoluteName()+" because its type is null");
+			throw new IllegalArgumentException("Can't give sql type for column " + column.getAbsoluteName() + " because its type is null");
 		}
 		if (column instanceof SizedColumn) {
 			int size = ((SizedColumn) column).getSize();
@@ -49,32 +58,33 @@ public class JavaTypeToSqlTypeMapping {
 	}
 	
 	/**
-	 * Renvoie le type SQL par défaut pour une classe Java
-	 * @param javaType
-	 * @return
+	 * Gives the SQL type of a Java class
+	 *
+	 * @param javaType a Java class
+	 * @return the SQL type for the given column
 	 */
-	public String getTypeName(Class javaType) {
+	private String getTypeName(Class javaType) {
 		String type = defaultJavaTypeToSQLType.get(javaType);
 		if (type == null) {
-			throw new IllegalArgumentException("No sql type defined for "+javaType);
+			throw new IllegalArgumentException("No sql type defined for " + javaType);
 		}
 		return type;
 	}
 	
 	/**
-	 * Renvoie le type SQL le plus proche pour une classe Java
-	 * @param javaType
-	 * @param size
-	 * @return
+	 * Gives the learest SQL type of a Java class according to the expected size
+	 *
+	 * @param javaType a Java class
+	 * @return the SQL type for the given column
 	 */
-	public String getTypeName(Class javaType, Integer size) {
+	String getTypeName(Class javaType, Integer size) {
 		if (size == null) {
 			return getTypeName(javaType);
 		} else {
 			SortedMap<Integer, String> typeNames = javaTypeToSQLType.get(javaType).tailMap(size);
 			String typeName = Iterables.firstValue(typeNames);
 			if (typeName != null) {
-				// NB: on utilise $l comme Hibernate pour simplifier une éventuelle transition
+				// NB: we use $l as Hibernate to ease an eventual switch between framworks
 				typeName = typeName.replace("$l", String.valueOf(size));
 			} else {
 				typeName = getTypeName(javaType);
