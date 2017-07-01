7d2f9b4a3b0da79c250c99bdd7bc33cfc2bdb39f
Guillaume Mary <guiom.mary@gmail.com>
2017-07-01 09:34:38 +0200

    Smarter PersistableIdentifier#toString()

--- orm/src/main/java/org/gama/stalactite/persistence/id/PersistableIdentifier.java
+++ orm/src/main/java/org/gama/stalactite/persistence/id/PersistableIdentifier.java
@@ -35,4 +35,9 @@ public class PersistableIdentifier<T> extends AbstractIdentifier<T> {
 			return false;
 		}
 	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + " = " + getSurrogate();
+	}
 }
