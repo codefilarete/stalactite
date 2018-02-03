e37d09870247b4b77ab6542d36c61f0856a95084
Guillaume Mary <guiom.mary@gmail.com>
2018-02-03 13:23:56 +0100

    Simple Connection provider from a DataSource

---
+++ sql/src/main/java/org/gama/sql/DataSourceConnectionProvider.java
@@ -0,0 +1,28 @@
+package org.gama.sql;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Implementation that gives the conneciton from an underlying {@link DataSource}
+ * 
+ * @author Guillaume Mary
+ */
+public class DataSourceConnectionProvider implements ConnectionProvider {
+	
+	private final DataSource dataSource;
+	
+	public DataSourceConnectionProvider(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+	
+	@Override
+	public Connection getCurrentConnection() {
+		try {
+			return dataSource.getConnection();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
