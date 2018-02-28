51a48107355ad8db62fecb97bee146fca73869b4
Guillaume Mary <guiom.mary@gmail.com>
2018-02-28 22:22:52 +0100

    Better Javadoc

--- sql/src/main/java/org/gama/sql/ConnectionProvider.java
+++ sql/src/main/java/org/gama/sql/ConnectionProvider.java
@@ -1,11 +1,20 @@
 package org.gama.sql;
 
+import javax.annotation.Nonnull;
 import java.sql.Connection;
 
 /**
+ * A simple contract to give the eventually existing {@link Connection}
+ * 
  * @author Guillaume Mary
  */
 public interface ConnectionProvider {
 	
+	/**
+	 * Gives an eventually existing {@link Connection} or opens a new one.
+	 * 
+	 * @return not null
+	 */
+	@Nonnull
 	Connection getCurrentConnection();
 }
