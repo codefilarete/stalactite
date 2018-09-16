a1040967d3f0fbcf983af945f980dc1b299000ef
Guillaume Mary <guiom.mary@gmail.com>
2018-09-16 16:13:25 +0200

    Adding table ownership to PrimaryKey

--- core/src/main/java/org/gama/stalactite/persistence/structure/PrimaryKey.java
+++ core/src/main/java/org/gama/stalactite/persistence/structure/PrimaryKey.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Set;
 
+import org.gama.lang.collection.Iterables;
 import org.gama.lang.collection.KeepOrderSet;
 
 /**
@@ -13,14 +14,21 @@ import org.gama.lang.collection.KeepOrderSet;
  */
 public class PrimaryKey<T extends Table> {
 	
+	private final T table;
 	private final Set<Column<T, Object>> columns = new KeepOrderSet<>();
 	
 	public PrimaryKey(Collection<Column<T, Object>> columns) {
+		this.table = Iterables.first(columns).getTable();
 		this.columns.addAll(columns);
 	}
 	
 	public PrimaryKey(Column<T, Object> column, Column<T, Object> ... additionalColumns) {
+		this.table = column.getTable();
 		this.addColumns(column, additionalColumns);
+	}
+	
+	public T getTable() {
+		return table;
 	}
 	
 	public void addColumns(Column<T, Object> column, Column<T, Object> ... additionalColumns) {
