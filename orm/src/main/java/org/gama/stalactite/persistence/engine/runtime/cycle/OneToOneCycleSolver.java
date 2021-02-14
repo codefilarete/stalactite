c08da7eff4f2fc5a4fe76ff4e509eb694e711ebd
Guillaume Mary <guiom.mary@gmail.com>
2021-02-14 09:08:29 +0100

    code cleanup

--- orm/src/main/java/org/gama/stalactite/persistence/engine/runtime/cycle/OneToOneCycleSolver.java
+++ orm/src/main/java/org/gama/stalactite/persistence/engine/runtime/cycle/OneToOneCycleSolver.java
@@ -13,40 +13,40 @@ import org.gama.stalactite.persistence.engine.configurer.PersisterBuilderImpl.Po
 import org.gama.stalactite.persistence.engine.runtime.IEntityConfiguredJoinedTablesPersister;
 
 public class OneToOneCycleSolver<SRC, TRGT> extends PostInitializer<TRGT> {
-		
-		private final Set<CyclingPersisterSolver<?, ?>> cycleSolvers = new LinkedHashSet<>();
-		
-		public OneToOneCycleSolver(Class<TRGT> entityType) {
-			super(entityType);
-		}
-		
-		public void addCycleSolver(IReversibleAccessor<SRC, TRGT> targetProvider,
-									CascadeOneConfigurer<SRC, TRGT, ?, ?> cascadeOneConfigurer) {
-			this.cycleSolvers.add(new CyclingPersisterSolver<>(targetProvider, cascadeOneConfigurer));
-		}
-		
-		@Override
-		public void consume(IEntityConfiguredJoinedTablesPersister<TRGT, Object> targetPersister) {
-			List<ConfigurationResult> configurationResults = new ArrayList<>();
-			OneToOneCycleLoader<SRC, TRGT, Object> oneToOneCycleLoader = new OneToOneCycleLoader<>(targetPersister);
-			targetPersister.addSelectListener(oneToOneCycleLoader);
-			cycleSolvers.forEach((CyclingPersisterSolver c) -> {
-				ConfigurationResult configurationResult = c.cascadeOneConfigurer.appendCascadesWith2PhasesSelect(targetPersister, oneToOneCycleLoader);
-				oneToOneCycleLoader.addRelation(c.relationName, configurationResult);
-				configurationResults.add(configurationResult);
-				
-			});
-		}
-		
-		private class CyclingPersisterSolver<SRC, SRCID> {
+	
+	private final Set<CyclingPersisterSolver<?>> cycleSolvers = new LinkedHashSet<>();
+	
+	public OneToOneCycleSolver(Class<TRGT> entityType) {
+		super(entityType);
+	}
+	
+	public void addCycleSolver(IReversibleAccessor<SRC, TRGT> targetProvider,
+							   CascadeOneConfigurer<SRC, TRGT, ?, ?> cascadeOneConfigurer) {
+		this.cycleSolvers.add(new CyclingPersisterSolver<>(targetProvider, cascadeOneConfigurer));
+	}
+	
+	@Override
+	public void consume(IEntityConfiguredJoinedTablesPersister<TRGT, Object> targetPersister) {
+		List<ConfigurationResult> configurationResults = new ArrayList<>();
+		OneToOneCycleLoader<SRC, TRGT, Object> oneToOneCycleLoader = new OneToOneCycleLoader<>(targetPersister);
+		targetPersister.addSelectListener(oneToOneCycleLoader);
+		cycleSolvers.forEach((CyclingPersisterSolver c) -> {
+			ConfigurationResult configurationResult = c.cascadeOneConfigurer.appendCascadesWith2PhasesSelect(targetPersister, oneToOneCycleLoader);
+			oneToOneCycleLoader.addRelation(c.relationName, configurationResult);
+			configurationResults.add(configurationResult);
 			
-			private final String relationName;
-			private final CascadeOneConfigurer<SRC, TRGT, SRCID, Object> cascadeOneConfigurer;
-			
-			public CyclingPersisterSolver(IReversibleAccessor<SRC, TRGT> targetProvider,
-										  CascadeOneConfigurer<SRC, TRGT, SRCID, ?> cascadeOneConfigurer) {
-				this.relationName = AccessorDefinition.toString(targetProvider);
-				this.cascadeOneConfigurer = (CascadeOneConfigurer<SRC, TRGT, SRCID, Object>) cascadeOneConfigurer;
-			}
+		});
+	}
+	
+	private class CyclingPersisterSolver<SRCID> {
+		
+		private final String relationName;
+		private final CascadeOneConfigurer<SRC, TRGT, SRCID, Object> cascadeOneConfigurer;
+		
+		public CyclingPersisterSolver(IReversibleAccessor<SRC, TRGT> targetProvider,
+									  CascadeOneConfigurer<SRC, TRGT, SRCID, ?> cascadeOneConfigurer) {
+			this.relationName = AccessorDefinition.toString(targetProvider);
+			this.cascadeOneConfigurer = (CascadeOneConfigurer<SRC, TRGT, SRCID, Object>) cascadeOneConfigurer;
 		}
 	}
+}
