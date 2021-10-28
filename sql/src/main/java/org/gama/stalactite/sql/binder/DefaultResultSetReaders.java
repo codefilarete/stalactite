c94b4fe4d39f58682529064244b1917c44c3c09d
Guillaume Mary <guiom.mary@gmail.com>
2021-10-28 23:35:07 +0200

    Code cleanup : replacing interface by final class

--- sql/src/main/java/org/gama/stalactite/sql/binder/DefaultResultSetReaders.java
+++ sql/src/main/java/org/gama/stalactite/sql/binder/DefaultResultSetReaders.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
  *
  * @author Guillaume Mary
  */
-public interface DefaultResultSetReaders {
+public final class DefaultResultSetReaders {
 	
 	/* Implementation note: although primitive binders are parameterized with generics they are not wrapped by a NullAwareResultSetReader
 	 * so they'll throw an exception or convert Object-typed value passed if null is writen or read.
@@ -23,126 +23,129 @@ public interface DefaultResultSetReaders {
 	/**
 	 * {@link ResultSetReader} for {@link ResultSet#getLong(String)}.
 	 */
-	ResultSetReader<Long> LONG_PRIMITIVE_READER = ResultSet::getLong;
+	public static final ResultSetReader<Long> LONG_PRIMITIVE_READER = ResultSet::getLong;
 	
 	/**
 	 * {@link ResultSetReader} for {@link ResultSet#getLong(String)}.
 	 * Wrapped into a {@link NullAwareResultSetReader} to manage type boxing and unboxing.
 	 */
-	ResultSetReader<Long> LONG_READER = new NullAwareResultSetReader<>(LONG_PRIMITIVE_READER);
+	public static final ResultSetReader<Long> LONG_READER = new NullAwareResultSetReader<>(LONG_PRIMITIVE_READER);
 	
 	/**
 	 * {@link ResultSetReader} for {@link ResultSet#getInt(String)}.
 	 */
-	ResultSetReader<Integer> INTEGER_PRIMITIVE_READER = ResultSet::getInt;
+	public static final ResultSetReader<Integer> INTEGER_PRIMITIVE_READER = ResultSet::getInt;
 	
 	/**
 	 * {@link ResultSetReader} for {@link ResultSet#getInt(String)}.
 	 * Wrapped into a {@link NullAwareResultSetReader} to manage type boxing and unboxing.
 	 */
-	ResultSetReader<Integer> INTEGER_READER = new NullAwareResultSetReader<>(INTEGER_PRIMITIVE_READER);
+	public static final ResultSetReader<Integer> INTEGER_READER = new NullAwareResultSetReader<>(INTEGER_PRIMITIVE_READER);
 	
 	/**
 	 * {@link ResultSetReader} for {@link ResultSet#getByte(String)}.
 	 */
-	ResultSetReader<Byte> BYTE_PRIMITIVE_READER = ResultSet::getByte;
+	public static final ResultSetReader<Byte> BYTE_PRIMITIVE_READER = ResultSet::getByte;
 	
 	/**
 	 * {@link ResultSetReader} for {@link ResultSet#getByte(String)}.
 	 * Wrapped into a {@link NullAwareResultSetReader} to manage type boxing and unboxing.
 	 */
-	ResultSetReader<Byte> BYTE_READER = new NullAwareResultSetReader<>(BYTE_PRIMITIVE_READER);
+	public static final ResultSetReader<Byte> BYTE_READER = new NullAwareResultSetReader<>(BYTE_PRIMITIVE_READER);
 	
 	/**
 	 * {@link ResultSetReader} for {@link ResultSet#getBytes(String)}.
 	 */
-	ResultSetReader<byte[]> BYTES_READER = ResultSet::getBytes;
+	public static final ResultSetReader<byte[]> BYTES_READER = ResultSet::getBytes;
 	
 	/**
 	 * {@link ResultSetReader} for {@link ResultSet#getDouble(String)}.
 	 */
-	ResultSetReader<Double> DOUBLE_PRIMITIVE_READER = ResultSet::getDouble;
+	public static final ResultSetReader<Double> DOUBLE_PRIMITIVE_READER = ResultSet::getDouble;
 	
 	/**
 	 * {@link ResultSetReader} for {@link ResultSet#getDouble(String)}.
 	 * Wrapped into a {@link NullAwareResultSetReader} to manage type boxing and unboxing.
 	 */
-	ResultSetReader<Double> DOUBLE_READER = new NullAwareResultSetReader<>(DOUBLE_PRIMITIVE_READER);
+	public static final ResultSetReader<Double> DOUBLE_READER = new NullAwareResultSetReader<>(DOUBLE_PRIMITIVE_READER);
 	
 	/**
 	 * {@link ResultSetReader} for {@link ResultSet#getFloat(String)}.
 	 */
-	ResultSetReader<Float> FLOAT_PRIMITIVE_READER = ResultSet::getFloat;
+	public static final ResultSetReader<Float> FLOAT_PRIMITIVE_READER = ResultSet::getFloat;
 	
 	/**
 	 * {@link ResultSetReader} for {@link ResultSet#getFloat(String)}.
 	 * Wrapped into a {@link NullAwareResultSetReader} to manage type boxing and unboxing.
 	 */
-	ResultSetReader<Float> FLOAT_READER = new NullAwareResultSetReader<>(FLOAT_PRIMITIVE_READER);
+	public static final ResultSetReader<Float> FLOAT_READER = new NullAwareResultSetReader<>(FLOAT_PRIMITIVE_READER);
 	
 	/**
 	 * {@link ResultSetReader} for {@link ResultSet#getBigDecimal(String)}.
 	 */
-	ResultSetReader<BigDecimal> BIGDECIMAL_READER = new NullAwareResultSetReader<>(ResultSet::getBigDecimal);
+	public static final ResultSetReader<BigDecimal> BIGDECIMAL_READER = new NullAwareResultSetReader<>(ResultSet::getBigDecimal);
 	
 	/**
 	 * {@link ResultSetReader} for {@link ResultSet#getBoolean(String)}.
 	 */
-	ResultSetReader<Boolean> BOOLEAN_PRIMITIVE_READER = ResultSet::getBoolean;
+	public static final ResultSetReader<Boolean> BOOLEAN_PRIMITIVE_READER = ResultSet::getBoolean;
 	
 	/**
 	 * {@link ResultSetReader} for {@link ResultSet#getBoolean(String)}.
 	 * Wrapped into a {@link NullAwareResultSetReader} to manage type boxing and unboxing.
 	 */
-	ResultSetReader<Boolean> BOOLEAN_READER = new NullAwareResultSetReader<>(BOOLEAN_PRIMITIVE_READER);
+	public static final ResultSetReader<Boolean> BOOLEAN_READER = new NullAwareResultSetReader<>(BOOLEAN_PRIMITIVE_READER);
 	
 	/**
 	 * {@link ResultSetReader} for {@link ResultSet#getDate(String)}.
 	 * For common usage, prefer {@link DateBinder} because it uses {@link java.util.Date}
 	 * @see DateBinder
 	 */
-	ResultSetReader<Date> DATE_SQL_READER = ResultSet::getDate;
+	public static final ResultSetReader<Date> DATE_SQL_READER = ResultSet::getDate;
 	
 	/**
 	 * {@link ResultSetReader} for {@link ResultSet#getTimestamp(String)}.
 	 */
-	ResultSetReader<Timestamp> TIMESTAMP_READER = ResultSet::getTimestamp;
+	public static final ResultSetReader<Timestamp> TIMESTAMP_READER = ResultSet::getTimestamp;
 	
 	/**
 	 * {@link ResultSetReader} for {@link ResultSet#getString(String)}.
 	 */
-	ResultSetReader<String> STRING_READER = ResultSet::getString;
+	public static final ResultSetReader<String> STRING_READER = ResultSet::getString;
 	
 	/**
 	 * {@link ResultSetReader} for {@link ResultSet#getBinaryStream(String)}.
 	 * @see DerbyParameterBinders#BINARYSTREAM_BINDER
 	 * @see HSQLDBParameterBinders#BINARYSTREAM_BINDER
 	 */
-	ResultSetReader<InputStream> BINARYSTREAM_READER = ResultSet::getBinaryStream;
+	public static final ResultSetReader<InputStream> BINARYSTREAM_READER = ResultSet::getBinaryStream;
 	
 	/**
 	 * {@link ResultSetReader} for {@link ResultSet#getBlob(String)}.
 	 */
-	ResultSetReader<Blob> BLOB_READER = ResultSet::getBlob;
+	public static final ResultSetReader<Blob> BLOB_READER = ResultSet::getBlob;
 	
 	/**
 	 * {@link ResultSetReader} for {@link java.util.Date}
 	 */
-	ResultSetReader<java.util.Date> DATE_READER = new NullAwareResultSetReader<>(new DateBinder());
+	public static final ResultSetReader<java.util.Date> DATE_READER = new NullAwareResultSetReader<>(new DateBinder());
 	
 	/**
 	 * {@link ResultSetReader} for {@link java.time.LocalDate}
 	 */
-	ResultSetReader<LocalDate> LOCALDATE_READER = new NullAwareResultSetReader<>(new LocalDateBinder());
+	public static final ResultSetReader<LocalDate> LOCALDATE_READER = new NullAwareResultSetReader<>(new LocalDateBinder());
 	
 	/**
 	 * {@link ResultSetReader} for {@link java.time.LocalDateTime}
 	 */
-	ResultSetReader<LocalDateTime> LOCALDATETIME_READER = new NullAwareResultSetReader<>(new LocalDateTimeBinder());
+	public static final ResultSetReader<LocalDateTime> LOCALDATETIME_READER = new NullAwareResultSetReader<>(new LocalDateTimeBinder());
 	
 	/**
 	 * {@link ResultSetReader} for {@link Object}
 	 */
-	ResultSetReader<Object> OBJECT_READER = ResultSet::getObject;
+	public static final ResultSetReader<Object> OBJECT_READER = ResultSet::getObject;
 	
+	private DefaultResultSetReaders() {
+		// Class for constants
+	}
 }
